48ccecc4458596e42a6cd588ac4412c36704ab82
MaAnka324 <matviychuk.anna324@gmail.com>
2023-05-26 18:30:57 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,143 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import Reset from "./components/Reset";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Settings from "./components/Set";
+import {useDispatch, useSelector} from "react-redux";
+import {increment, increment0, max, set, start} from "./redux/counterReducer";
+import {RootState} from "./redux/store";
 
 function App() {
 
-    let [start, setStart] = useState<any>(JSON.parse(localStorage.getItem('startValue') ?? '0'))
-    let [max, setMax] = useState<any>(JSON.parse(localStorage.getItem('maxValue') ?? '5'))
-    let [displayValue, setDisplayValue] = useState<any>( JSON.parse(localStorage.getItem('counterValue') ?? '0'))//localStorage.getItem('counterValue') ? localStorage.getItem('counterValue'): '0'
-    let [disableSet, setDisableSet] = useState<boolean>(false)
-    let [disableInc, setDisableInc] = useState<boolean>(false)
-    let [disableReset, setDisableReset] = useState<boolean>(false)
+    // let [start, setStart] = useState<any>(JSON.parse(localStorage.getItem('startValue') ?? '0'))
+    // let [max, setMax] = useState<any>(JSON.parse(localStorage.getItem('maxValue') ?? '5'))
+    // let [displayValue, setDisplayValue] = useState<any>( JSON.parse(localStorage.getItem('counterValue') ?? '0'))    //localStorage.getItem('counterValue') ? localStorage.getItem('counterValue'): '0'
 
-    useEffect( () => {
-        localStorage.setItem('counterValue', JSON.stringify(displayValue))
-    }, [displayValue] )
+    const value = useSelector<RootState, number>(state => state.counter.value)
 
-    useEffect( () => {
-        localStorage.setItem('startValue', JSON.stringify(start))
-    }, [start] )
+    const disableInc = useSelector<RootState, boolean>(state => state.counter.disableInc)
 
-    useEffect( () => {
-        localStorage.setItem('maxValue', JSON.stringify(max))
-    }, [max] )
+    const disableSet = useSelector<RootState, boolean>(state => state.counter.disableSet)
+
+    const startValue = useSelector<RootState, number>(state => state.counter.startValue)
+
+    const maxValue = useSelector<RootState, number>(state => state.counter.maxValue)
+
+    // let [start, setStart] = useState<any>(0)
+    // let [max, setMax] = useState<any>(5)
+    // let [disableSet, setDisableSet] = useState<boolean>(false)
+
+    // let [displayValue, setDisplayValue] = useState<number>(0)
+    // let [disableInc, setDisableInc] = useState<boolean>(false)
+    // let [disableReset, setDisableReset] = useState<boolean>(false)
+
+    // useEffect( () => {
+    //     localStorage.setItem('counterValue', JSON.stringify(displayValue))
+    // }, [displayValue] )
+    //
+    // useEffect( () => {
+    //     localStorage.setItem('startValue', JSON.stringify(start))
+    // }, [start] )
+    //
+    // useEffect( () => {
+    //     localStorage.setItem('maxValue', JSON.stringify(max))
+    // }, [max] )
+
+    const dispatch = useDispatch()
+
+
 
     const startDisable = (e: ChangeEvent<HTMLInputElement>) => {
-        handleStartValue(+e.currentTarget.value)
-        setDisableInc(true)
-        setDisableReset(true)
+        // handleStartValue(+e.currentTarget.value)
+        // setDisableInc(true)
+        // setDisableReset(true)
+        dispatch(start(+e.currentTarget.value))
     }
 
     const maxDisable = (e: ChangeEvent<HTMLInputElement>) => {
-        handleMaxValue(+e.currentTarget.value)
-        setDisableInc(true)
-        setDisableReset(true)
+        // handleMaxValue(+e.currentTarget.value)
+        // setDisableInc(true)
+        // setDisableReset(true)
+        dispatch(max(+e.currentTarget.value))
     }
 
     const handleStartValue = (newStartValue: number) => {
-        setStart(newStartValue)
-        setDisableSet(false)
-        if(newStartValue < 0) {
-            setDisableSet(true)
-            setDisplayValue('Incorrect value')
-            return
-        }
-        if(newStartValue >=  max) {
-            setDisableSet(true)
-            setDisplayValue('Incorrect value')
-            return
-        }
-        else {
-            return  setDisplayValue('Click set')
-        }
+        // setStart(newStartValue)
+        // setDisableSet(false)
+        // if (newStartValue < 0) {
+        //     setDisableSet(true)
+        //     setDisplayValue('Incorrect value')
+        //     return
+        // }
+        // if (newStartValue >= max) {
+        //     setDisableSet(true)
+        //     setDisplayValue('Incorrect value')
+        //     return
+        // } else {
+        //     return setDisplayValue('Click set')
+        // }
     }
 
     const handleMaxValue = (newMaxValue: number) => {
-        setMax(newMaxValue)
-        setDisableSet(false)
-        if(newMaxValue < 0) {
-            setDisableSet(true)
-            setDisplayValue('Incorrect value')
-            return
-        }
-        if(newMaxValue <= start) {
-            setDisableSet(true)
-            setDisplayValue('Incorrect value')
-            return
-        }
-        else return setDisplayValue('Click set')
+        // setMax(newMaxValue)
+        // setDisableSet(false)
+        // if (newMaxValue < 0) {
+        //     setDisableSet(true)
+        //     setDisplayValue('Incorrect value')
+        //     return
+        // }
+        // if (newMaxValue <= start) {
+        //     setDisableSet(true)
+        //     setDisplayValue('Incorrect value')
+        //     return
+        // }
+        //  else return setDisplayValue('Click set')
     }
 
     const handlerDisplayValue = () => {
-        setDisplayValue(start)
-        // setDisableSet(true)
-        setDisableInc(false)
-        setDisableReset(false)
+        // setDisplayValue(start)
+        // setDisableInc(false)
+        // setDisableReset(false)
+        dispatch(set())
     }
 
     const onClickHandler = () => {
-        if (start === max) return
-        setDisplayValue(++displayValue)
-        if(displayValue === max) setDisableInc(true)
+        // if (start === max) return
+        // setDisplayValue(++displayValue)
+        // if (displayValue === max) setDisableInc(true)
+        dispatch(increment())
     }
 
     const onClickHandler0 = () => {
-        setDisplayValue(start)
-        setDisableInc(false)
+        // setDisplayValue(start)
+        // setDisableInc(false)
+        dispatch(increment0())
     }
 
-  return (
-      <BrowserRouter>
-        <Routes>
-            <Route path={'/*'} element={<Settings
-                max={max}
-                start={start}
-                handleStartValue={handleStartValue}
-                handleMaxValue={handleMaxValue}
-                handlerDisplayValue={handlerDisplayValue}
-                onChangeStartDis={startDisable}
-                onChangeMaxDis={maxDisable}
-                disabled={disableSet}
-            />}/>
-            <Route path={'/reset'} element={ <Reset
-                displayValue={displayValue}
-                onClickHandler={onClickHandler}
-                onClickHandler0={onClickHandler0}
-                disableInc={disableInc}
-                disableReset={disableReset}
-                max={max}
-            />}/>
-        </Routes>
-      </BrowserRouter>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path={'/*'} element={<Settings
+                    max={maxValue}
+                    start={startValue}
+                    handleStartValue={handleStartValue}
+                    handleMaxValue={handleMaxValue}
+                    handlerDisplayValue={handlerDisplayValue}
+                    onChangeStartDis={startDisable}
+                    onChangeMaxDis={maxDisable}
+                    disabled={disableSet}
+                />}/>
+                <Route path={'/reset'} element={<Reset
+                    // displayValue={displayValue}
+                    displayValue={value.toString()}
+                    onClickHandler={onClickHandler}
+                    onClickHandler0={onClickHandler0}
+                    disableInc={disableInc}
+                    disableReset={disableSet}
+                    max={maxValue.toString()}
+                />}/>
+            </Routes>
+        </div>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +13,11 @@ const root = ReactDOM.createRoot(
 root.render(
 
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 
 );
