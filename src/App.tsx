1b7a58c76b495261b4f8b92ff833ad7e9c666867
MaAnka324 <matviychuk.anna324@gmail.com>
2023-08-08 19:18:47 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,17 @@ function App() {
 
     return (
         <div>
-           {/* <Routes>
-                <Route path={'/*'} element={<Settings/>}/>
-                <Route path={'/reset'} element={<Reset/>}/>
-            </Routes>
-*/}
+            {/*{<Routes>*/}
+            {/*    <Route path={'/'} element={<Settings/>}/>*/}
+            {/*    <Route path={'/reset'} element={<Reset/>}/>*/}
+            {/*</Routes>*/}
+            {/*}*/}
 
             <Routes>
                 <Route path="/" >
                     <Route index element={<Settings />} />
                     <Route path="reset" element={<Reset />} />
+                    <Route path='*' element={<h1>404: PAGE NOT FOUND</h1>} />
                 </Route>
             </Routes>
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 root.render(
 
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
           <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 
 )
